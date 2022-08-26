a3dbdae2feb918de04f9124d7cef4551643cd34d
Fabboy <fabboynice@gmail.com>
2022-08-26 12:23:44 +0200

    Added Vector2f, System functions, Colors, Added some other features and bug fixxes

---
+++ engine/includes/Colors.h
@@ -0,0 +1,50 @@
+
+#ifndef GAME_COLORS_H
+#define GAME_COLORS_H
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+SDL_Color White = {255, 255, 255};
+SDL_Color Black = {0, 0, 0};
+SDL_Color Red = {255, 0, 0};
+SDL_Color Green = {0, 255, 0};
+SDL_Color Yellow = {255, 255, 0};
+SDL_Color Blue = {0, 0, 255};
+SDL_Color Magenta = {255, 0, 255};
+SDL_Color Cyan = {0, 255, 255};
+SDL_Color LightGray = {211, 211, 211};
+SDL_Color Gray = {128, 128, 128};
+SDL_Color DarkGray = {169, 169, 169};
+SDL_Color LightRed = {255, 160, 122};
+SDL_Color LightGreen = {173, 255, 47};
+SDL_Color LightYellow = {255, 255, 224};
+SDL_Color LightBlue = {173, 216, 230};
+SDL_Color LightMagenta = {255, 182, 193};
+SDL_Color LightCyan = {224, 255, 255};
+SDL_Color LightWhite = {255, 255, 255};
+SDL_Color Violette = {238, 130, 238};
+SDL_Color Brown = {165, 42, 42};
+SDL_Color Orange = {255, 165, 0};
+SDL_Color Pink = {255, 192, 203};
+SDL_Color Salmon = {250, 128, 114};
+SDL_Color LightSalmon = {255, 160, 122};
+SDL_Color DarkSalmon = {233, 150, 122};
+SDL_Color LightCoral = {240, 128, 128};
+SDL_Color Tomato = {255, 99, 71};
+SDL_Color DarkOrange = {255, 140, 0};
+SDL_Color OrangeRed = {255, 69, 0};
+SDL_Color HotPink = {255, 105, 180};
+SDL_Color DeepPink = {255, 20, 147};
+SDL_Color LightPink = {255, 182, 193};
+SDL_Color PaleVioletRed = {219, 112, 147};
+SDL_Color MediumVioletRed = {199, 21, 133};
+SDL_Color Crimson = {220, 20, 60};
+SDL_Color RedViolet = {186, 85, 211};
+SDL_Color MediumOrchid = {186, 85, 211};
+SDL_Color DarkOrchid = {153, 50, 204};
+SDL_Color DarkViolet = {148, 0, 211};
+SDL_Color BlueViolet = {138, 43, 226};
+SDL_Color Purple = {160, 32, 240};
+SDL_Color MediumPurple = {147, 112, 219};
+SDL_Color Thistle = {216, 191, 216};
+
+#endif //GAME_COLORS_H
